78d90aa3fdba0712ac8c92eb412dfb310232a054
mhaktanacar <56204447+mhaktanacar@users.noreply.github.com>
2020-05-18 12:27:07 +0300

    Hibernate and Database Connections

--- src/main/java/com/mhacar/entity/Student.java
+++ src/main/java/com/mhacar/entity/Student.java
@@ -1,24 +1,97 @@
 package com.mhacar.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
-@Table(schema = "TBL_STUDENT")
+@Table(name ="student")
 public class Student {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
-
-    @Column(name="first_name")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
     private String firstName;
-
-    @Column(name="last_name")
     private String lastName;
-
-    @Column(name="email")
     private String email;
 
+    public Student() {
+        super();
+    }
 
+    public Student(Long id, String name, String passportNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student(String name, String passportNumber) {
+        super();
+        this.name = name;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Student [id=%s, name=%s, passportNumber=%s]", id, name);
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = true, length = 45)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name", nullable = true, length = 45)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 45)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) &&
+                Objects.equals(firstName, student.firstName) &&
+                Objects.equals(lastName, student.lastName) &&
+                Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email);
+    }
 }
